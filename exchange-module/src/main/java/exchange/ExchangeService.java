9452bea72a07ddbc708d2cc9f444844fc4cf57c2
shamil256git <shamilbaker50@gmail.com>
2025-04-06 17:38:44 +0300

    commit #4
    in NV from line  33 to 51 a function to return  currency rate

--- exchange-module/src/main/java/exchange/ExchangeService.java
+++ exchange-module/src/main/java/exchange/ExchangeService.java
@@ -30,3 +30,22 @@ public class ExchangeService {
         exchangeRates.put("JPY-EUR", 0.0077);
         exchangeRates.put("JPY-GBP", 0.0066);
     }
+    public double getExchangeRate(String fromCurrency, String toCurrency) {
+        // If same currency, rate is 1.0
+        if (fromCurrency.equals(toCurrency)) {
+            return 1.0;
+        }
+
+        String key = fromCurrency + "-" + toCurrency;
+        Double rate = exchangeRates.get(key);
+
+        // Return the rate or 0.0 if not found
+        return (rate != null) ? rate : 0.0;
+    }
+
+
+    public void addExchangeRate(String fromCurrency, String toCurrency, double rate) {
+        String key = fromCurrency + "-" + toCurrency;
+        exchangeRates.put(key, rate);
+    }
+}
